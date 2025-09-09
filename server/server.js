1f6eebea1453c442c2ee2b5e949222cd7353361d
EgzonBllacaa <egzonbllaca9@gmail.com>
2025-09-09 17:26:24 +0200

    server update

--- server/server.js
+++ server/server.js
@@ -11,8 +11,17 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin:
-      "http://localhost:5173" || "https://social-media-sigma-two.vercel.app",
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://social-media-sigma-two.vercel.app",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
