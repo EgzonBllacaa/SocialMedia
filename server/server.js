bce6cfb27758d2de4161f32ed40de19197b52b12
EgzonBllacaa <egzonbllaca9@gmail.com>
2025-09-09 17:37:39 +0200

    bug fix

--- server/server.js
+++ server/server.js
@@ -6,15 +6,24 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT;
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://social-media-sigma-two.vercel.app",
+];
 
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://social-media-sigma-two.vercel.app",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
