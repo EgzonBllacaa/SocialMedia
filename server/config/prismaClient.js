98bb970a50e7eba219dd89a51e08717b87d393d1
EgzonBllacaa <egzonbllaca9@gmail.com>
2025-09-09 20:10:01 +0200

    railway_database

--- server/config/prismaClient.js
+++ server/config/prismaClient.js
@@ -1,6 +1,16 @@
+// prisma.js
 import { PrismaClient } from "@prisma/client";
 
-const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+const globalForPrisma = global;
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"], // optional: helps debug
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
